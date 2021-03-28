e99cd98f555f9a827e7d8dd8cba6d42d56f46d90
adam <tangzhixiang1991@gmail.com>
2021-03-28 22:35:13 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  base: '/vuepress-blog/',
+  title: 'Hello VuePress',
+  description: '前端笔记',
+    head: [
+        ['link', 
+            { rel: 'icon', href: '' }
+        ],  
+    ],
+}
