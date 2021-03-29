18abd24d3a44236c93e9a1d22e57180a68b3a4c3
zhixiang.tang <zhixiang.tang@jwis.cn>
2021-03-29 15:34:11 +0800

    no message

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,12 @@
 module.exports = {
   base: '/vuepress-blog/',
-  title: 'Hello VuePress',
   description: '前端笔记',
-    head: [
-        ['link', 
-            { rel: 'icon', href: '' }
-        ],  
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'GitHub', link: 'https://google.com' },
     ],
+    sidebar: [
+      ]
+  }
 }
