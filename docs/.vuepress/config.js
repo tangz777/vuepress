2f6a045155427d9a5fb4a3dac46094fdc9b1bdee
ty-zebra <ty-zebra@mail.idcduty.com>
2022-09-05 22:22:19 +0800

    no message

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,42 +1,102 @@
 module.exports = {
-  base: '/vuepress-blog/',
-  description: '前端笔记',
+  base: "/vuepress-blog/",
+  description: "前端笔记",
   head: [
     [
-      'link',
+      "link",
       {
-        rel: 'icon',
-        href: '/icon.png',
+        rel: "icon",
+        href: "/icon.png",
       },
     ],
   ],
   themeConfig: {
-    lastUpdated: '上次更新',
+    lastUpdated: "上次更新",
     nav: [
       {
-        text: '首页',
-        link: '/',
+        text: "首页",
+        link: "/",
       },
       {
-        text: '面试题集',
-        link: '/interview/',
+        text: "前端知识点总结",
+        link: "/interview/",
         // items: [
         //   { text: 'HTML', link: '/interview/html/' },
         //   { text: 'CSS', link: '/interview/css/' },
         //   { text: 'JavaScript', link: '/interview/JavaScript/' },
         // ],
       },
+      {
+        text: "阅读",
+      },
+      {
+        text: "每日一题",
+      },
+      {
+        text: "tools",
+      },
     ],
+    // sidebar: [
+    //   {
+    //     title: '前端知识点总结',
+    //     collapsable: false,
+    //     sidebarDepth:1,
+    //     children: [
+    //       '/pages/html/',
+    //       '/pages/css/',
+    //       '/pages/JavaScript/',
+    //     ],
+    //   },
+    // ],
     sidebar: [
       {
-        title: '面试题集',
-        collapsable: false,
+        title: "前端基础",
+        collapsable: true,
+        sidebarDepth: 1,
         children: [
-          '/interview/html/',
-          '/interview/css/',
-          '/interview/JavaScript/',
+          {
+            title: "基础篇",
+            collapsable: false,
+            sidebarDepth: 1,
+            children: [
+              {
+                title: "1 html模块",
+                collapsable: true,
+                sidebarDepth: 1,
+                children: [
+                  ["pages/folder1/html/1.md", "html模块1"],
+                  ["pages/folder1/html/2.md", "html模块2"],
+                ],
+              },
+              {
+                title: "2 CSS模块",
+                collapsable: true,
+                sidebarDepth: 1,
+                children: [
+                  ["pages/folder1/css/1.md", "CSS模块1"],
+                  ["pages/folder1/css/2.md", "CSS模块2"],
+                ],
+              },
+            ],
+          },
         ],
+        // "/pages/css/": [
+        //   {
+        //     title: "测试菜单1", // 必要的
+        //     collapsable: false, // 可选的, 默认值是 true,
+        //     sidebarDepth: 1, // 可选的, 默认值是 1
+        //     children: [
+        //       ["1.md", "子菜单1"],
+        //       ["3.md", "子菜单2"],
+        //     ],
+        //   },
+        //   {
+        //     title: "测试菜单2",
+        //     collapsable: false, // 可选的, 默认值是 true,
+        //     children: [["2.md", "子菜单2"]],
+        //   },
+        // ],
       },
     ],
   },
-}
+};
--- docs/README.md
+++ docs/README.md
@@ -4,7 +4,7 @@ heroImage: /main.png
 heroText: null
 tagline: null
 actionText: start →
-actionLink: /interview/
+actionLink: /pages/
 features:
   - title: 简洁至上
     details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
